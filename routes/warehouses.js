b5aae187b208a1f88906f1efad061dd5e26f3c4f
abeyra <63668913+abeyra@users.noreply.github.com>
2022-03-16 17:26:51 -0400

    update warehouse route
    put router.delete at the bottom of page.

--- routes/warehouses.js
+++ routes/warehouses.js
@@ -28,37 +28,6 @@ function writeInventory(data) {
 }
 //////////
 
-
-//This route will delete a warehouse and all it's associated inventory
-router.delete("/delete/:id", (req, res) => {
-  const warehouses = readWarehouses();
-  const inventories = readInventory();
-
-  const warehouseIndex = warehouses.findIndex((warehouse) => {
-    return warehouse.id === req.params.id;
-  });
-  
-  const deletedWarehouse = warehouses[warehouseIndex] 
-
-  if (warehouseIndex === -1){
-    return res.status(404).send(`Warehouse ID: ${req.params.id} NOT Found!`);
-  }else{
-    //New Warehouse file with the deleted warehouse cut out
-    warehouses.splice(warehouseIndex, 1); 
-    
-    //New Inventories file with all of the deleted warehouse inventory items cut out.
-    const newInv = inventories.filter(inv => {
-      return inv.warehouseID !== req.params.id
-    })
-    
-    //Write Inventory and Warehouse information to json files
-    writeInventory(newInv)
-    writeWarehouses(warehouses)
-  } 
-  
-  res.status(202).send(`Deleted - Warehouse: ${deletedWarehouse.name} - ${deletedWarehouse.id}, in ${deletedWarehouse.city},  Successfully`);
-});
-
 router.get("/", (req, res) => {
   const warehouses = readWarehouses();
 
@@ -89,7 +58,36 @@ router.get('/:id', (req, res) => {
   res.json(individualWarehouse);
 });
 
-
 //This route returns all warehouse data from the json data file to the user
+
+//This route will delete a warehouse and all it's associated inventory
+router.delete("/delete/:id", (req, res) => {
+  const warehouses = readWarehouses();
+  const inventories = readInventory();
+
+  const warehouseIndex = warehouses.findIndex((warehouse) => {
+    return warehouse.id === req.params.id;
+  });
+  
+  const deletedWarehouse = warehouses[warehouseIndex] 
+
+  if (warehouseIndex === -1){
+    return res.status(404).send(`Warehouse ID: ${req.params.id} NOT Found!`);
+  }else{
+    //New Warehouse file with the deleted warehouse cut out
+    warehouses.splice(warehouseIndex, 1); 
+    
+    //New Inventories file with all of the deleted warehouse inventory items cut out.
+    const newInv = inventories.filter(inv => {
+      return inv.warehouseID !== req.params.id
+    })
+    
+    //Write Inventory and Warehouse information to json files
+    writeInventory(newInv)
+    writeWarehouses(warehouses)
+  } 
+  
+  res.status(202).send(`Deleted - Warehouse: ${deletedWarehouse.name} - ${deletedWarehouse.id}, in ${deletedWarehouse.city},  Successfully`);
+});
 
 module.exports = router;
